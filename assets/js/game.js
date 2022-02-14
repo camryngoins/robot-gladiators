e2b4b4ed6bf41d49e2d6cd8bcbadfa2e092bb79d
camryngoins <camryngoins00@gmail.com>
2022-02-14 15:27:21 -0600

    setting up arrays

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,19 @@
+// Ganes States
+// "WIN" - Player robot has defeated all enemy-robots
+//  *Fight all enemby-robots
+//  * Defeat each enemy-robot 
+// "LOSE" - Player robot's health is zero or less 
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -59,4 +68,4 @@ var fight = function() {
     }
   }
 }
-fight();
+// fight();
