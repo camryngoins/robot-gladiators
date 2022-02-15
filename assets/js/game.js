e11678c17db053cab229ba3b11b0dcc0ca2f49f1
camryngoins <camryngoins00@gmail.com>
2022-02-15 16:47:19 -0600

    loop can defeat all enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -61,9 +63,12 @@ var fight = function (enemyName) {
         // if no (false), ask question again by running fight() again
         else {
             fight();
-        }
+      }
     }
+  }
 }
 for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
