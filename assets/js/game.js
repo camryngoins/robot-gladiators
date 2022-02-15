763edcc59bd4e9b8eeac51cb00cd290f878454ae
camryngoins <camryngoins00@gmail.com>
2022-02-15 11:56:16 -0600

    adding for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,13 @@ console.log(enemyNames);
 console.log(enemyNames[0]);
 console.log(enemyNames[1]);
 console.log(enemyNames[2]);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
